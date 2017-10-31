08182502e9c91e926723bd8faf9bcbe65adc63d0
Muhammad Tahir <mtahir.ps@gmail.com>
2017-10-31 11:12:24 +0500

    hello world

---
+++ helloWorld/server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+http.createServer(function(request,response) {
+    /**
+     * This will run when you hit localhost:1000/ in browser 
+     * or any tool like POSTMAN
+     */ 
+    response.end("Hello World");
+/**
+ * 1000 is port, you can set any available port
+ */ 
+}).listen(1000 , function () {
+    console.log("Server is running")
+})
+
+/**
+ * In folder "helloWorld" 
+ * You can run this file using following command
+ * node server.js
+ */ 
