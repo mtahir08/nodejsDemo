1243a88ea9487219f6ab2572158e59520614d4ff
Muhammad Tahir <mtahir.ps@gmail.com>
2017-11-01 10:08:05 +0500

    express middleware

---
+++ express-middleware/origin.js
@@ -0,0 +1,39 @@
+// import modules
+const Express = require("express");
+const logger = require('morgan');
+// initialize express app
+let app = Express();
+const port = 3001
+
+
+// Middleware
+app.use((req, res, next) => {
+    console.log("method " + req.method + " to " + req.url);
+    next();
+})
+app.use(logger('dev'));
+// Allow origins
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+    res.header('Access-Control-Allow-Credentials', "true");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    next();
+});
+
+
+// Routing
+app.get('/', (req, res) => {
+    /**
+     * req == request
+     * res == response
+     * */
+    res.status(200).send("<h1> Hello Express</h1>");
+})
+
+// Starting server
+app.listen(port, () => {
+    console.log("server is running!!")
+});
+// 
