d2afc6b735ed180b859e0e2dc9db21e00178d1fb
Muhammad Tahir <mtahir.ps@gmail.com>
2018-08-10 17:57:36 +0500

    files and packages updated

--- helloWorld/server.js
+++ helloWorld/server.js
@@ -1,19 +1,20 @@
 const http = require('http');
-http.createServer((request, response) => {
-    /**
-     * This will run when you hit localhost:1000/ in browser 
-     * or any tool like POSTMAN
-     */
-    response.end("Hello World");
-    /**
-     * 1000 is port, you can set any available port
-     */
-}).listen(1000, () => {
-    console.log("Server is running")
-})
+const server = http.createServer(function(request, response) {
+  /**
+   * This will run when you hit localhost:3000/ in browser
+   * or any tool like POSTMAN
+   */
+  response.end('Hello World');
+});
+/**
+ * 3000 is port, you can set any available port
+ */
+server.listen(3000, function() {
+  console.log('Server is running');
+});
 
 /**
- * In folder "helloWorld" 
+ * In folder "helloWorld"
  * You can run this file using following command
- * node server.js
+ * node server.js or npm start
  */
