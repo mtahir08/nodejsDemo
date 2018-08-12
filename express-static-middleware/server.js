5184a09a55aac8bc80f44194562df1604150f7e3
Muhammad Tahir <mtahir.ps@gmail.com>
2018-08-13 01:43:14 +0500

    static files updated with fetch

--- express-static-middleware/server.js
+++ express-static-middleware/server.js
@@ -1,29 +1,44 @@
 // import modules
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 // initialize express app
 let app = express();
-const port = 3001
-
+const port = 3001;
 
 // Middleware
-var publicPath = path.resolve(__dirname, "public");
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+// static
+var publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
-
 
 // Routing
 app.get('/', (req, res) => {
-    // res.sendFile(path.resolve(__dirname, "public","index.html"));
-    /**
-     * If you don't set static folder, and send file directly from sendDile 
-     * then you could't get all files in static foler 
-     */ 
-    res.sendFile("index.html");
-})
+  // res.sendFile(path.resolve(__dirname, "public","index.html"));
+  /**
+   * If you don't set static folder, and send file directly from sendFile
+   * then you could't get all files in static foler
+   */
+
+  res.sendFile('index.html');
+});
+
+app.get('/form', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'form.html'));
+});
+
+app.post('/add', (req, res) => {
+  console.log('DATA RECEIVED FROM CLIENT', req.body);
+  const obj = req.body;
+  obj['message'] = 'Data received from client';
+  res.send(obj);
+});
 
 // Starting server
 app.listen(port, () => {
-    console.log("server is running!!")
+  console.log('server is running!!');
 });
-// 
