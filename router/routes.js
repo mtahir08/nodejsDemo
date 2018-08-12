2ffa0d07ccda9da3c16fc327d291724567978a8c
Muhammad Tahir <mtahir.ps@gmail.com>
2018-08-13 01:46:00 +0500

    routes updated

--- router/routes.js
+++ router/routes.js
@@ -1,22 +1,23 @@
-var express = require("express");
+var express = require('express');
 
 var api = express.Router();
 
-api.get("/users", function (req, res) {
-    res.send(`in users and ${req.method} method`)
-});
-api.post("/user", function (req, res) {
-    res.send(`in users and ${req.method} method`)
-
-});
-api.get("/messages", function (req, res) {
-    res.send(`in message and ${req.method} method`)
-
-});
-api.post("/message", function (req, res) {
-    res.send(`in message and ${req.method} method`)
-
+api.get('/users', function(req, res) {
+  res.send(`in users and ${req.method} method`);
 });
 
+api.post('/user', function(req, res) {
+  console.log('route /user body', req.body);
+  res.send(`in users and ${req.method} method`);
+});
+
+api.get('/messages', function(req, res) {
+  res.send(`in message and ${req.method} method`);
+});
+
+api.post('/message', function(req, res) {
+  console.log('route /message body', req.body);
+  res.send(`in message and ${req.method} method`);
+});
 
 module.exports = api;
--- router/server.js
+++ router/server.js
@@ -1,27 +1,29 @@
 // import modules
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 /**
- * Our app will need to post new guestbook entries in HTTP POST requests, 
+ * Our app will need to post in HTTP POST requests,
  * so we'll need to parse the body of the POST; that's where body will come in
  */
 
-const bodyParser = require("body-parser");
 /**
  * Here we are importing our routes;
- */ 
-var apiRouter = require("./routes.js");
+ */
+var apiRouter = require('./routes.js');
 // initialize express app
 let app = express();
 const port = 3001;
-app.use(bodyParser.urlencoded({ extended: false })) 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", apiRouter)
+// parse application/json
+app.use(bodyParser.json());
 
+app.use('/', apiRouter);
 
 // Starting server
 app.listen(port, () => {
-    console.log("server is running!!")
+  console.log('server is running!!');
 });
-// 
+//
