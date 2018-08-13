76db4472920c49550e6afa47740c33020c20125c
Muhammad Tahir <mtahir.ps@gmail.com>
2018-08-13 15:47:03 +0500

    mongoose require

--- mongodb/mongodb.js
+++ mongodb/mongodb.js
@@ -1,6 +1,6 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://tahir:tahir@ds053305.mlab.com:53305/scotch";
+// var MongoClient = require('mongodb').MongoClient;
 const mongoose = require("mongoose");
+var url = "mongodb://localhost:27017/my_db";
 
 // MongoClient.connect(url, function (err, db) {
 //   if (err) {
