8f900eb5af0d10ca8737ac604be2634c242848d9
Muhammad Tahir <mtahir.ps@gmail.com>
2019-10-27 01:19:17 +0500

    heroku

---
+++ server.js
@@ -0,0 +1,40 @@
+// import modules
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+// initialize express app
+let app = express();
+const port = process.env.PORT || 3001;
+/**
+ * Here we are importing our routes;
+ */
+
+require('./class-app/mongodb');
+const routers = require('./class-app/routes.js');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+app.get('/', (req, res) => {
+  res.send({ message: 'success' });
+});
+// app.use('/api', routers);
+app.use('/auth', routers);
+
+// Starting server
+app.listen(port, () => {
+  console.log(`server is running on port ${port}!!`);
+});
+//
