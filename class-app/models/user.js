06ec500ad9367151c3a1a4d8fe607705ffbcfda0
Muhammad Tahir <mtahir.ps@gmail.com>
2019-10-27 00:48:04 +0500

    update user schema

--- class-app/models/user.js
+++ class-app/models/user.js
@@ -3,9 +3,13 @@ const Schema = mongoose.Schema;
 
 const userSchema = Schema({
   name: { type: String, required: true },
+  dob: { type: String },
+  picture: { type: String },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   gender: { type: String, enum: ['male', 'female'], default: 'male' },
+  createdAt: { type: Date, default: Date.now() },
+  updatedAt: { type: Date, default: Date.now() }
 });
 
 const User = mongoose.model('User', userSchema);
