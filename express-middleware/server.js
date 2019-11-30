ef7842e2d9d6b05b0b1209bb0e05b3b3d004ec1c
Muhammad Tahir <mtahir.ps@gmail.com>
2019-11-30 23:48:56 +0500

    version update

--- express-middleware/server.js
+++ express-middleware/server.js
@@ -1,6 +1,5 @@
 // import modules
 const Express = require("express");
-const logger = require('morgan');
 // initialize express app
 let app = Express();
 const port = 3001
@@ -32,23 +31,13 @@ const port = 3001
  * the request will be left hanging.
  * 
  * 
- */ 
+ */
 
 // Middleware
 app.use((req, res, next) => {
     console.log("method " + req.method + " to " + req.url);
     next();
 })
-// app.use(logger('short'));
-// Allow origins
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', "http://localhost:3000");
-//     res.header('Access-Control-Allow-Credentials', "true");
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
-//     next();
-// });
 
 
 // Routing
@@ -64,4 +53,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log("server is running!!")
 });
-// 
+//
