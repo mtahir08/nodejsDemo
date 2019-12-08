fc1266f16f069a9dbc8573709e804d59c09f3660
Muhammad Tahir <mtahir.ps@gmail.com>
2019-12-08 12:05:47 +0500

    add multer

---
+++ storage/server.js
@@ -0,0 +1,18 @@
+const Express = require('express')
+const multer = require('multer')
+const bodyParser = require('body-parser')
+
+const port = 3001;
+const app = Express();
+const upload = multer({ dest: 'uploads/' })
+
+app.use(bodyParser.json());
+
+app.post('/file', upload.single('profile'), (req, res) => {
+    console.log("req.file", req.file);
+    res.send({ status: true })
+})
+
+app.listen(port, () => {
+    console.log("server is running!!")
+})
