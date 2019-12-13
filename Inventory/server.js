b25c46d07e4b2091b9600878bedafc63ceed9960
Muhammad Tahir <mtahir.ps@gmail.com>
2019-12-14 03:12:15 +0500

    env add

--- Inventory/server.js
+++ Inventory/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3000;
 
 require('./mongodb');
 const authRouter = require('./routes/auth');
