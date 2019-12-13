0308d736e61270c1899864f3de990b672c5651a4
Muhammad Tahir <mtahir.ps@gmail.com>
2019-12-14 03:17:29 +0500

    login

--- Inventory/controllers/auth.js
+++ Inventory/controllers/auth.js
@@ -1,6 +1,6 @@
 
 const Users = require('./../services/User');
-
+const JWT = require('./../services/JWT');
 module.exports = {
     Signup: async (req, res) => {
         const obj = req.body;
@@ -19,7 +19,25 @@ module.exports = {
         }
     },
     Login: async (req, res) => {
-
-        res.status(200).send({ message: "" })
+        try {
+            const obj = req.body;
+            let user = await Users.getByEmail(obj.email)
+            if (user) {
+                const comparePass = Users.compare(user.password, obj.password)
+                if (comparePass) {
+                    // to convert from mongoose instance to js object
+                    const token = JWT.generateToken(user)
+                    user = user.toObject()
+                    delete user.password
+                    return res.status(200).send({ data: { user, token }, message: "Successfully Login" });
+                } else {
+                    return res.status(409).send({ message: "Email or pass not matched" });
+                }
+            }
+            return res.status(404).send({ user });
+        } catch (error) {
+            console.log("error", error);
+            res.status(500).send({ error });
+        }
     },
 }
--- Inventory/routes/auth.js
+++ Inventory/routes/auth.js
@@ -4,6 +4,7 @@ const Auth = require('./../controllers/auth');
 const api = express.Router();
 
 api.post('/signup', Auth.Signup)
+api.post('/signin', Auth.Login)
 
 
 module.exports = api;
