57718962025cf54f776fd88c1af72f59df8d4d52
Muhammad Tahir <mtahir.ps@gmail.com>
2019-12-15 22:42:09 +0500

    inventory model

---
+++ Inventory/models/inventory.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const inventorySchema = new Schema({
+    name: { type: String, required: true },
+    cagetory: { type: String, required: true },
+    qty: { type: Number, required: true },
+    price: { type: Number, required: true },
+    picture: { type: String },
+    createdBy: { type: Schema.Types.ObjectId, ref: "User" },
+    createdAt: { type: Date, default: Date.now() },
+    updatedAt: { type: Date, default: Date.now() }
+});
+
+const Inventory = mongoose.model('Inventory', inventorySchema);
+module.exports = Inventory;
