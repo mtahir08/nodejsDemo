5f6310052ec223653aa19ac702ae6bd89238c7c8
Muhammad Tahir <mtahir.ps@gmail.com>
2019-12-21 01:17:50 +0500

    get inventory

--- Inventory/controllers/inventory.js
+++ Inventory/controllers/inventory.js
@@ -17,4 +17,20 @@ module.exports = {
             res.status(500).send({ error: "Please try again" });
         }
     },
+    getInventory: async (req, res) => {
+        try {
+            const { id } = req.params;
+            if (!id) {
+                return res.status(500).send({ error: "Please try again" });
+            }
+            let item = await Inventories.getInventory({ _id: id })
+            if (item) {
+                return res.status(200).send({ data: item, message: "Item Found" });
+            }
+            return res.status(404).send({ data: {}, message: "Item not found." });
+        } catch (error) {
+            console.log("error", error);
+            res.status(500).send({ error: "Please try again" });
+        }
+    },
 }
