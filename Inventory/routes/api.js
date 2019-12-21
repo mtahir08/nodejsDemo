2417ea27ec935f38d75f7b9d0d3fa098baa59fa9
Muhammad Tahir <mtahir.ps@gmail.com>
2019-12-21 22:22:56 +0500

    add auth guard

--- Inventory/routes/api.js
+++ Inventory/routes/api.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const Inventory = require('./../controllers/inventory');
 const Users = require('./../controllers/users');
+const Authorization = require('./../services/Authorization');
 
 const api = express.Router();
 
 /**    INVENTORY    **/
-api.post('/inventory', Inventory.AddInventory)
-api.get('/inventory/:type?', Inventory.getInventory)
+api.post('/inventory', Authorization, Inventory.AddInventory)
+api.get('/inventory/:type?', Authorization, Inventory.getInventory)
 // api.get('/inventories', Inventory.getInventories)
-api.put('/inventory', Inventory.updateInventory)
-api.delete('/inventory', Inventory.removeInventory)
+api.put('/inventory', Authorization, Inventory.updateInventory)
+api.delete('/inventory', Authorization, Inventory.removeInventory)
 
 
 /**    USERS    **/
-api.get('/users/:type?', Users.GetUsers)
+api.get('/users/:type?', Authorization, Users.GetUsers)
 
 
 module.exports = api;
---
+++ Inventory/services/Authorization.js
@@ -0,0 +1,25 @@
+
+const JWT = require('./JWT');
+const Authorization = (req, res, next) => {
+    try {
+        const authHeader = req.get('Authorization')
+        if (!authHeader) {
+            throw new Error('Authorization failed!')
+        }
+        const token = authHeader.split(' ').pop()
+        if (!token || token === '') {
+            throw new Error('Authorization failed!')
+        }
+        const decoded = JWT.verify(token)
+        if (!decoded) {
+            throw new Error('Authorization failed!')
+        }
+        req.isAuthenticated = true;
+        next();
+    } catch (error) {
+        req.isAuthenticated = false;
+        return res.status(401).send({ message: typeof error === 'string' ? error : 'Authorization failed!' })
+    }
+}
+
+module.exports = Authorization
