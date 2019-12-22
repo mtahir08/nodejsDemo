27d6082c35c7de1b525ee8ef8a4e7eb7a1a0f399
Muhammad Tahir <mtahir.ps@gmail.com>
2019-12-22 14:06:24 +0500

    cors middleware

--- Inventory/server.js
+++ Inventory/server.js
@@ -13,6 +13,17 @@ const apiRouter = require('./routes/api');
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: false, }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
+
 app.use('/images', express.static('uploads'));
 app.use('/auth', authRouter);
 app.use('/api', apiRouter);
