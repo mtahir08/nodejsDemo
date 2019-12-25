e31262473c81ed01d44a0074d4ca3b8979cbad48
Muhammad Tahir <mtahir.ps@gmail.com>
2019-12-25 22:32:34 +0500

    db change

--- Inventory/mongodb.js
+++ Inventory/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const url = "mongodb://localhost:27017/inventory";
-
+// const url = "mongodb://localhost:27017/inventory";
+const url = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds137368.mlab.com:${process.env.DB_PORT}/${process.env.DB}`;
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'CONNECTION ERROR :'));
