18f7dba2162553b5dc28b37fe75b5f6e34d19bd3
Muhammad Tahir <mtahir.ps@gmail.com>
2019-12-26 00:24:34 +0500

    reupload

--- fileChangeFunction/functions/index.js
+++ fileChangeFunction/functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const gcs = require("@google-cloud/storage")
+const os = require('os')
+const path = require('path')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,8 +9,26 @@ const functions = require('firebase-functions');
 exports.onFileChange = functions.storage.object().onFinalize((event) => {
     console.log(event);
     const object = event.data;
-    const bucket = evnet.bucket;
+    const bucket = object.bucket;
     const contentType = object.contentType;
     const filePath = object.name;
-    return;
+
+    if (path.basename(filePath).startsWith('renamed-')) {
+        console.log("we already have this file");
+        return;
+    }
+
+    const destBucket = gcs.bucket(bucket)
+    const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath))
+    const metadata = { contentType }
+    return destBucket.file(file).download({
+        destination: tmpFilePath
+    })
+        .then(() => {
+            destBucket.upload({
+                destination: 'renamed-' + path.basename(filePath),
+                metadata
+            })
+        })
+        .catch(() => { })
 });
