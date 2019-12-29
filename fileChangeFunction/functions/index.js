355e31dc845b071dac74f799da4244954ae12ab3
Muhammad Tahir <mtahir.ps@gmail.com>
2019-12-29 15:43:16 +0500

    reupload file

--- fileChangeFunction/functions/index.js
+++ fileChangeFunction/functions/index.js
@@ -3,7 +3,6 @@ const os = require('os')
 const path = require('path')
 const admin = require('firebase-admin');
 admin.initializeApp()
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -11,21 +10,25 @@ exports.onFileChange = functions.storage.object().onFinalize((object) => {
     const bucket = object.bucket;
     const contentType = object.contentType;
     const filePath = object.name;
-
-    if (path.basename(filePath).startsWith('renamed-')) {
+    const fileName = path.basename(filePath);
+    if (fileName.startsWith('renamed-')) {
         console.log("we already have this file");
         return;
     }
 
     const destBucket = admin.storage().bucket(bucket)
-    const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath))
+    const tempFilePath = path.join(os.tmpdir(), fileName);
     const metadata = { contentType }
     return destBucket.file(filePath).download({
-        destination: tmpFilePath
+        destination: tempFilePath
     })
-        .then(() => {
-            return destBucket.upload({
-                destination: 'renamed-' + path.basename(filePath),
+        .then(async () => {
+            console.log('Thumbnail created at', tempFilePath);
+            const thumbFileName = `renamed-${fileName}`;
+            const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
+
+            return destBucket.upload(tempFilePath, {
+                destination: thumbFilePath,
                 metadata
             })
         })
