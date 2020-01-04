fbefae49df885715016bddcb8d8df8e45614e15c
Muhammad Tahir <mtahir.ps@gmail.com>
2020-01-04 14:04:35 +0500

    google cloud storage

--- fileChangeFunction/functions/index.js
+++ fileChangeFunction/functions/index.js
@@ -8,47 +8,47 @@ admin.initializeApp()
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.onFileChange = functions.storage.object().onFinalize((object) => {
-    const bucket = object.bucket;
-    const contentType = object.contentType;
-    const filePath = object.name;
+exports.onFileChange = functions.storage.object().onFinalize(async (object) => {
+    const fileBucket = object.bucket; // The Storage bucket that contains the file.
+    const contentType = object.contentType; // File content type.
+    const filePath = object.name; // File path in the bucket.
     const fileName = path.basename(filePath);
-
+    // If image not exists
     if (fileName.resourcdState === 'not_exists') {
         console.log("we deleted this file. exit...")
         return;
 
     }
-    if (fileName.startsWith('renamed-')) {
-        console.log("we already have this file");
+    // Exit if the image is already a thumbnail.
+    if (fileName.startsWith('thumb_')) {
+        console.log('Already a Thumbnail.');
         return;
     }
     // Exit if this is triggered on a file that is not an image.
     if (!contentType.startsWith('image/')) {
-        return console.log('This is not an image.');
+        console.log('This is not an image.');
+        return;
     }
 
-    const destBucket = admin.storage().bucket(bucket)
+
+
+    // Download file from bucket.
+    const bucket = admin.storage().bucket(fileBucket);
     const tempFilePath = path.join(os.tmpdir(), fileName);
-    const metadata = { contentType }
-    return destBucket.file(filePath).download({
-        destination: tempFilePath
-    })
-        .then(() => {
-            return spawn('convert', [tempFilePath, '-resize', '180x180', tempFilePath])
-        })
-        .then(() => {
-            console.log('Thumbnail created at', tempFilePath);
-            const thumbFileName = `renamed-${fileName}`;
-            // const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
-            return destBucket.upload(tempFilePath, {
-                destination: thumbFileName,
-                metadata
-            })
-        })
-        .then(() => {
-            // Once the thumbnail has been uploaded delete the local file to free up disk space.
-            return fs.unlinkSync(tempFilePath);
-        })
-        .catch((error) => { console.log(error); return; })
+    const metadata = { contentType };
+    await bucket.file(filePath).download({ destination: tempFilePath });
+    console.log('Image downloaded locally to', tempFilePath);
+    // Generate a thumbnail using ImageMagick.
+    await spawn('convert', [tempFilePath, '-thumbnail', '200x200>', tempFilePath]);
+    console.log('Thumbnail created at', tempFilePath);
+    // We add a 'thumb_' prefix to thumbnails file name. That's where we'll upload the thumbnail.
+    const thumbFileName = `thumb_${fileName}`;
+    const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
+    // Uploading the thumbnail.
+    await bucket.upload(tempFilePath, {
+        destination: thumbFilePath,
+        metadata: metadata,
+    });
+    // Once the thumbnail has been uploaded delete the local file to free up disk space.
+    return fs.unlinkSync(tempFilePath);
 });
